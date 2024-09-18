bbf95fc879fc0b4d0387953426565ecce28a4c3a
Nathan <nathan.chopin76@gmail.com>
2024-09-18 13:48:32 +0200

    mofif main

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ print( '\n test de la fonction nombre de jour dans le mois:')
 print('L\' pour l\'année 2023 le mois février avais :',fn.nombre_jour(2,2023) ,' jours')
 print('L\' pour l\'année 2000 le mois février avais :',fn.nombre_jour(2,2000),' jours')
 
+print( '\n test de la vérification de la date:')
+
+print('La date du 36/02/549 est : ', fn.vérification_date(36,2,546))
+print('La date du 17/09/2024 est : ',fn.vérification_date(17,9,2024))
+
+
+
+
