28487eadfbf6e7cf98c0532bdc15e7f0f15a71ae
Nathan <nathan.chopin76@gmail.com>
2024-09-19 11:35:51 +0200

    mofif multiplication

--- exo3/fn_exo3.py
+++ exo3/fn_exo3.py
@@ -1,12 +1,14 @@
 # fonction de l'exercice 3 du tp 1
 # Nathan Chopin
 # 19/09/24
+# pas fini
+
 
 def multiplication(A,B):
     '''multiplication de matrice carrée'''
     C = [[0]*len(A)]*len(A)
-    for a in range(len(A)):
-        for b in range(len(B)):
-            for c in range(len(A)):
-                C[b][a] += A[b][c] * B[c][a]
+    for i in range(len(A)):
+        for j in range(len(B)):
+            for k in range(len(A)):
+                C[i][j] += A[i][k] * B[k][j]
     return C
