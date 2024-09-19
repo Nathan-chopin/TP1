29a93d8ed5b6b92e2cd89bc45e01f782f495aaae
Nathan <nathan.chopin76@gmail.com>
2024-09-19 11:30:51 +0200

    ajout fn multiplica tion et son test

--- exo3/fn_exo3.py
+++ exo3/fn_exo3.py
@@ -1,3 +1,12 @@
 # fonction de l'exercice 3 du tp 1
 # Nathan Chopin
 # 19/09/24
+
+def multiplication(A,B):
+    '''multiplication de matrice carrée'''
+    C = [[0]*len(A)]*len(A)
+    for a in range(len(A)):
+        for b in range(len(B)):
+            for c in range(len(A)):
+                C[b][a] += A[b][c] * B[c][a]
+    return C
--- exo3/main_exo3.py
+++ exo3/main_exo3.py
@@ -3,3 +3,8 @@
 # 19/09/24
 
 import fn_exo3 as fn
+
+A = [[2,4],[1,2]]
+B = [[1,0],[0,1]]
+
+print(fn.multiplication(A,B))
